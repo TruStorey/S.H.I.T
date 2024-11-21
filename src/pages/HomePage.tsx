8f98c363661c5fe4c329d0944b1bc1b067e5b14e
TruStorey <tech@neonmumbles.com>
2024-11-21 12:33:49 +0000

    Added card layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+  ListMinus,
+  CalendarClock,
+  Waypoints,
+  Network,
+  Binary,
+  ShieldCheck,
+  KeyRound,
+} from "lucide-react";
 import {
   Card,
   CardContent,
@@ -7,35 +17,86 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+
+const cardData = [
+  {
+    title: "Transform Lists",
+    description: "Edit and Transform Lists",
+    url: "/transform-list",
+    icon: ListMinus,
+  },
+  {
+    title: "Date/Time Convertor",
+    description: "Edit and Transform Lists",
+    url: "/date-time-converter",
+    icon: CalendarClock,
+  },
+  {
+    title: "Reverse Proxy Config",
+    description: "Edit and Transform Lists",
+    url: "/reverse-proxy-config",
+    icon: Waypoints,
+  },
+  {
+    title: "Visual Subnet Calculator",
+    description: "Edit and Transform Lists",
+    url: "/subnet-calculator",
+    icon: Network,
+  },
+  {
+    title: "Base64 String Convertor",
+    description: "Edit and Transform Lists",
+    url: "/base64-converter",
+    icon: Binary,
+  },
+  {
+    title: "Certificate Checker",
+    description: "Edit and Transform Lists",
+    url: "/certificate-checker",
+    icon: ShieldCheck,
+  },
+  {
+    title: "SSH Key Generator",
+    description: "Edit and Transform Lists",
+    url: "/ssh-key-generator",
+    icon: KeyRound,
+  },
+]
 
 const HomePage: React.FC = () => {
   return (
     <>
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="mb-4">
-        Small Helpful IT Tools
-      </h1>
-      <p>
-        A collection of s.h.i.t to help with the day to day, it's <a href="https://www.youtube.com/watch?v=edmqTODMZC4" target="_blank">happening</a>.
-      </p>
-    </div>
-    <Card className="w-[350px]">
-    <CardHeader>
-      <CardTitle>Create project</CardTitle>
-      <CardDescription>Deploy your new project in one-click.</CardDescription>
-    </CardHeader>
-    <CardContent>
-      This is a card
-    </CardContent>
-    <CardFooter className="flex justify-between">
-      <Button variant="outline">Cancel</Button>
-      <Button>Deploy</Button>
-    </CardFooter>
-  </Card>
-  </>
-  )
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="mb-4">Small Helpful IT Tools</h1>
+        <p>
+          A collection of s.h.i.t to help with the day to day, it's{" "}
+          <a href="https://www.youtube.com/watch?v=edmqTODMZC4" target="_blank">
+            happening
+          </a>
+          .
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-8 px-32"> 
+      {cardData.map((card, index) => ( 
+          
+        <Card key={index} className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>{card.title}</CardTitle>
+            <CardDescription>{card.description}</CardDescription>
+          </CardHeader>
+          <CardFooter className="flex justify-between">
+            <Button variant="default" size="icon" className="ml-auto" asChild>
+              <Link to={card.url}><card.icon /></Link>
+            </Button>
+          </CardFooter>
+        </Card>
+            
+        ))}
+        </div>
+      
+    </>
+  );
 };
-
 
 export default HomePage;
