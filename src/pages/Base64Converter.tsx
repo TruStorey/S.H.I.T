da28e4d4d853c38fd636e80d922092f01e2d7ad8
TruStorey <tech@neonmumbles.com>
2024-11-24 21:52:06 +0000

    New Base64Converter page

---
+++ src/pages/Base64Converter.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Binary } from "lucide-react";
+
+const Base64Converter: React.FC = () => {
+  return (
+    <>
+      <div className="p-4">
+        {/* Header */}
+        <Binary />
+        <h1>Encode or decode Base64 strings</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-8 p-4">
+        
+      </div>
+    </>
+  );
+};
+
+export default Base64Converter;
