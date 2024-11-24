069b2c005882261b2c5171c37be8a12eb67c9a2e
TruStorey <tech@neonmumbles.com>
2024-11-24 21:52:05 +0000

    Added Base64 Converter route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from "@/pages/HomePage";
 import TransformList from "@/pages/TransformList";
 import DateTimeConverter from "@/pages/DateTimeConverter";
 import SubnetCalculator from "@/pages/SubnetCalculator";
+import Base64Converter from "@/pages/Base64Converter";
 
 const App: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const App: React.FC = () => {
           <Route path="/transform-list" element={<TransformList />} />
           <Route path="/date-time-converter" element={<DateTimeConverter />} />
           <Route path="/subnet-calculator" element={<SubnetCalculator />} />
+          <Route path="/base64-converter" element={<Base64Converter />} />
         </Routes>
       </Layout>
     </Router>
