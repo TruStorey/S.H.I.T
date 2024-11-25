315ed4ee975463721ee129682e27589a64acd56d
TruStorey <tech@neonmumbles.com>
2024-11-25 00:12:48 +0000

    Updated boilerplate for new page

--- src/pages/BlankPage-Starter.tsx
+++ src/pages/BlankPage-Starter.tsx
@@ -4,12 +4,12 @@ import { CalendarClock } from "lucide-react";
 const DateTimeConverter: React.FC = () => {
   return (
     <>
-      <div className="p-4">
+      <div className="md:flex flex-1 p-4 gap-2 items-center justify-center">
         {/* Header */}
-        <CalendarClock />
+        <CalendarClock size="30"/>
         <h1>Convert dates and times into different formats or time zones.</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-8 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
         
       </div>
     </>
